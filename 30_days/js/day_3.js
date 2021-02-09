ac1c2adf8385cc0d405b9e2d382c0bfbbd3d9949
Robert De la Rosa <rdelarosa39@gmail.com>
2021-02-08 22:09:32 -0600

    Feat: JS day 3

---
+++ 30_days/js/day_3.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    // creates new array by filtering out the highest number and sorts by ascending
+    // then gets the last/highest number in the array
+    return nums.filter(num => num != Math.max(...nums)).sort((a,b) => a-b).pop();
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
---
+++ 30_days/js/day_3a.js
@@ -0,0 +1,48 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the reverseString function; it has one parameter, . You must perform the following actions:
+
+Try to reverse string  using the split, reverse, and join methods.
+If an exception is thrown, catch it and print the contents of the exception's  on a new line.
+Print  on a new line. If no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
+ */
+function reverseString(s) {
+	try{
+        s = s.split('').reverse().join('');
+    }catch(ex){
+        console.log("s.split is not a function");
+    }
+    finally{
+        console.log(s)
+    }
+}
+
+
+function main() {
+    const s = eval(readLine());
+    
+    reverseString(s);
+}
