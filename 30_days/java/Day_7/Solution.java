4b1744670b8c1916db635962995e048c319f593d
Robert De la Rosa <rdelarosa39@gmail.com>
2021-02-12 20:16:53 -0600

    Feat: day 7 java

---
+++ 30_days/java/Day_7/Solution.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.Collection;
+
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        List<Integer> arr = new ArrayList<>();
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr.add(arrItem);
+        }
+        Collections.reverse(arr);
+        for (int i : arr){
+            System.out.printf("%s ",i);
+        }
+        scanner.close();
+    }
+}
